f869faa53594c26b791aa24b5d73235c7b3d8712
Phuong Dao <phuong.dao@inkr.com>
2021-08-07 11:41:21 +0700

    add btn try it now

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -1,51 +1,48 @@
 export const Navigation = (props) => {
   return (
-    <nav id='menu' className='navbar navbar-default navbar-fixed-top'>
-      <div className='navbar-header'>
-          <button
-            type='button'
-            className='navbar-toggle collapsed'
-            data-toggle='collapse'
-            data-target='#bs-example-navbar-collapse-1'
-          >
-            {' '}
-            <span className='sr-only'>Toggle navigation</span>{' '}
-            <span className='icon-bar'></span>{' '}
-            <span className='icon-bar'></span>{' '}
-            <span className='icon-bar'></span>{' '}
-          </button>
-          <a className='navbar-brand page-scroll' href='#page-top'>
-            Online Exam
-          </a>{' '}
-          <ul className='nav navbar-nav navbar-right'>
-            <li>
-              <a href='#features' className='page-scroll'>
-                Tính năng
-              </a>
-            </li>
-            <li>
-              <a href='#about' className='page-scroll'>
-                Về chúng tôi
-              </a>
-            </li>
-            <li>
-              <a href='#services' className='page-scroll'>
-                Dịch vụ
-              </a>
-            </li>
-            <li>
-              <a href='#portfolio' className='page-scroll'>
-                Hình ảnh
-              </a>
-            </li>
-            <li>
-              <a href='#contact' className='page-scroll'>
-                Liên hệ
-              </a>
-            </li>
-          </ul>
-        </div>
-        
+    <nav id="menu" className="navbar navbar-default navbar-fixed-top">
+      <div className="navbar-header">
+        <a className="navbar-brand page-scroll header" href="#page-top">
+          Online Exam
+        </a>{" "}
+        <ul className="nav navbar-nav navbar-right">
+          <li>
+            <a href="#features" className="page-scroll">
+              Tính năng
+            </a>
+          </li>
+          <li>
+            <a href="#about" className="page-scroll">
+              Về chúng tôi
+            </a>
+          </li>
+          <li>
+            <a href="#services" className="page-scroll">
+              Dịch vụ
+            </a>
+          </li>
+          <li>
+            <a href="#portfolio" className="page-scroll">
+              Hình ảnh
+            </a>
+          </li>
+          <li>
+            <a href="#contact" className="page-scroll">
+              Liên hệ
+            </a>
+          </li>
+        </ul>
+      </div>
+
+      <a
+        href="https://online-exam-2021.herokuapp.com/"
+        class="glow-on-hover"
+        type="button"
+        target="_blank"
+        rel="noreferrer"
+      >
+        TRY IT NOW
+      </a>
     </nav>
-  )
-}
+  );
+};
