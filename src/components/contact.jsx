03e7a1652146987986624d897e64c98f484708fb
Phuong Dao <phuong.dao@inkr.com>
2021-08-09 09:52:15 +0700

    update ui form contact

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -1,143 +1,92 @@
-import { useState } from 'react'
-import emailjs from 'emailjs-com'
+import { useState } from "react";
+import emailjs from "emailjs-com";
 
 const initialState = {
-  name: '',
-  email: '',
-  message: '',
-}
+  name: "",
+  email: "",
+  message: "",
+};
 export const Contact = (props) => {
-  const [{ name, email, message }, setState] = useState(initialState)
+  const [{ name, email, message }, setState] = useState(initialState);
 
   const handleChange = (e) => {
-    const { name, value } = e.target
-    setState((prevState) => ({ ...prevState, [name]: value }))
-  }
-  const clearState = () => setState({ ...initialState })
+    const { name, value } = e.target;
+    setState((prevState) => ({ ...prevState, [name]: value }));
+  };
+  const clearState = () => setState({ ...initialState });
 
   const handleSubmit = (e) => {
-    e.preventDefault()
-    console.log(name, email, message)
+    e.preventDefault();
+    console.log(name, email, message);
     emailjs
-      .sendForm(
-        'YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', e.target, 'YOUR_USER_ID'
-      )
+      .sendForm("YOUR_SERVICE_ID", "YOUR_TEMPLATE_ID", e.target, "YOUR_USER_ID")
       .then(
         (result) => {
-          console.log(result.text)
-          clearState()
+          console.log(result.text);
+          clearState();
         },
         (error) => {
-          console.log(error.text)
+          console.log(error.text);
         }
-      )
-  }
+      );
+  };
   return (
     <div>
-      <div id='contact'>
-        <div className='container'>
-          <div className='col-md-8'>
-            <div className='row'>
-              <div className='section-title'>
-                <h2>Liên hệ với chúng tôi</h2>
-                <p>
-                  Vui lòng điền thông tin vào mẫu bên dưới, chúng tôi sẽ phản hồi lại cho bạn sớm nhất có thể
-                </p>
-              </div>
-              <form name='sentMessage' validate onSubmit={handleSubmit}>
-                <div className='row'>
-                  <div className='col-md-6'>
-                    <div className='form-group'>
-                      <input
-                        type='text'
-                        id='name'
-                        name='name'
-                        className='form-control'
-                        placeholder='Họ và tên'
-                        required
-                        onChange={handleChange}
-                      />
-                      <p className='help-block text-danger'></p>
-                    </div>
-                  </div>
-                  <div className='col-md-6'>
-                    <div className='form-group'>
-                      <input
-                        type='email'
-                        id='email'
-                        name='email'
-                        className='form-control'
-                        placeholder='Email'
-                        required
-                        onChange={handleChange}
-                      />
-                      <p className='help-block text-danger'></p>
-                    </div>
-                  </div>
-                </div>
-                <div className='form-group'>
-                  <textarea
-                    name='message'
-                    id='message'
-                    className='form-control'
-                    rows='4'
-                    placeholder='Nội dung'
-                    required
-                    onChange={handleChange}
-                  ></textarea>
-                  <p className='help-block text-danger'></p>
-                </div>
-                <div id='success'></div>
-                <button type='submit' className='btn btn-custom btn-lg'>
-                  Gửi
-                </button>
-              </form>
+      <div id="contact">
+        <div className="container">
+          <div className="col-md-8">
+            <div className="row">
+              <img
+                style={{ maxWidth: "100%" }}
+                src="/img/online-exam.png"
+                alt="online-exam"
+              />
             </div>
           </div>
-          <div className='col-md-3 col-md-offset-1 contact-info'>
-            <div className='contact-item'>
+          <div className="col-md-3 col-md-offset-1 contact-info">
+            <div className="contact-item">
               <h3>Thông tin liên hệ</h3>
               <p>
                 <span>
-                  <i className='fa fa-map-marker'></i> Địa chỉ
+                  <i className="fa fa-map-marker"></i> Địa chỉ
                 </span>
-                {props.data ? props.data.address : 'loading'}
+                {props.data ? props.data.address : "loading"}
               </p>
             </div>
-            <div className='contact-item'>
+            <div className="contact-item">
               <p>
                 <span>
-                  <i className='fa fa-phone'></i> Điện thoại
-                </span>{' '}
-                {props.data ? props.data.phone : 'loading'}
+                  <i className="fa fa-phone"></i> Điện thoại
+                </span>{" "}
+                {props.data ? props.data.phone : "loading"}
               </p>
             </div>
-            <div className='contact-item'>
+            <div className="contact-item">
               <p>
                 <span>
-                  <i className='fa fa-envelope-o'></i> Email
-                </span>{' '}
-                {props.data ? props.data.email : 'loading'}
+                  <i className="fa fa-envelope-o"></i> Email
+                </span>{" "}
+                {props.data ? props.data.email : "loading"}
               </p>
             </div>
           </div>
-          <div className='col-md-12'>
-            <div className='row'>
-              <div className='social'>
+          <div className="col-md-12">
+            <div className="row">
+              <div className="social">
                 <ul>
                   <li>
-                    <a href={props.data ? props.data.facebook : '/'}>
-                      <i className='fa fa-facebook'></i>
+                    <a href={props.data ? props.data.facebook : "/"}>
+                      <i className="fa fa-facebook"></i>
                     </a>
                   </li>
                   <li>
-                    <a href={props.data ? props.data.twitter : '/'}>
-                      <i className='fa fa-twitter'></i>
+                    <a href={props.data ? props.data.twitter : "/"}>
+                      <i className="fa fa-twitter"></i>
                     </a>
                   </li>
                   <li>
-                    <a href={props.data ? props.data.youtube : '/'}>
-                      <i className='fa fa-youtube'></i>
+                    <a href={props.data ? props.data.youtube : "/"}>
+                      <i className="fa fa-youtube"></i>
                     </a>
                   </li>
                 </ul>
@@ -146,13 +95,11 @@ export const Contact = (props) => {
           </div>
         </div>
       </div>
-      <div id='footer'>
-        <div className='container text-center'>
-          <p>
-            &copy; 2021 Developer Team.
-          </p>
+      <div id="footer">
+        <div className="container text-center">
+          <p>&copy; 2021 Developer Team.</p>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
